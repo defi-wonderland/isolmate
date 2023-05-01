877089c1fa0e3b037ebb29c9f1ad76ebdc1a2322
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2023-05-01 13:27:17 +0400

    style: run linter

--- src/tokens/ERC20.sol
+++ src/tokens/ERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import '../interfaces/tokens/IERC20.sol';
+import "../interfaces/tokens/IERC20.sol";
 
 /// @notice Modern and gas efficient ERC20 + EIP-2612 implementation.
 /// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC20.sol)
